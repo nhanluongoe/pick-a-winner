94c05e0f36d77bfeefc8dba78d2d33ea90ee513f
nhanluongoe <nhanlt18@gmail.com>
2024-01-15 18:56:50 +0700

    feat: add background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        main: "url('/bg.png')",
+      },
+    },
   },
   plugins: [],
 };
