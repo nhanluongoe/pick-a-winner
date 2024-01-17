61bd4f5414c25aa0fc021a18ce62b3c4105a1328
nhanluongoe <nhanlt18@gmail.com>
2024-01-17 10:39:51 +0700

    refactor: use arrow keys to navigate between stages instead

--- src/hooks/use-stage.ts
+++ src/hooks/use-stage.ts
@@ -1,23 +1,30 @@
 import { useState } from "react";
-import { useKeyPress } from "react-use";
+import { useKey } from "react-use";
 import usePrizes from "./use-prizes";
-
-const regex = /[0-9]/;
 
 export default function useStage() {
   const [stage, setStage] = useState<number>(0);
   const { prizes } = usePrizes();
 
-  useKeyPress((event) => {
-    const key = event.key;
+  useKey(
+    "ArrowRight",
+    () => {
+      if (stage + 1 > prizes.length - 1) return;
+      setStage((prev) => prev + 1);
+    },
+    {},
+    [stage],
+  );
 
-    if (regex.test(key)) {
-      if (parseInt(key) + 1 > prizes.length) return true;
-      setStage(parseInt(key));
-    }
-
-    return true;
-  });
+  useKey(
+    "ArrowLeft",
+    () => {
+      if (stage - 1 < 0) return;
+      setStage((prev) => prev - 1);
+    },
+    {},
+    [stage],
+  );
 
   return stage;
 }
